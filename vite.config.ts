6793e0b8d904ddbe3647d1e1eccb095cdd88f5f6
Ricardo Jaime Gil Simões <ricardo.gil.simoes@gmail.com>
2024-10-06 17:43:09 +0100

    chore: Package bumps

--- vite.config.ts
+++ vite.config.ts
@@ -53,4 +53,11 @@ export default defineConfig((env) => ({
       '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 }));
