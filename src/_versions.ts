702bb006d5582b0b8a2b0dede27fa084e8d2ac62
Ricardo Jaime Gil Simões <ricardo.gil.simoes@gmail.com>
2025-06-10 12:10:00 +0100

    chore: Package updates

--- src/_versions.ts
+++ src/_versions.ts
@@ -1,19 +1,19 @@
 export interface TsAppVersion {
-  version: string;
-  name: string;
-  description?: string;
-  versionLong?: string;
-  versionDate: string;
-  gitCommitHash?: string;
-  gitCommitDate?: string;
-  gitTag?: string;
-}
+    version: string;
+    name: string;
+    description?: string;
+    versionLong?: string;
+    versionDate: string;
+    gitCommitHash?: string;
+    gitCommitDate?: string;
+    gitTag?: string;
+};
 export const versions: TsAppVersion = {
-  version: '0.0.2',
-  name: 'ionic-react-kit',
-  versionDate: '2024-04-27T21:53:55.564Z',
-  description: 'An Ionic React Starter Kit bundled with care.',
-  gitCommitHash: '512ecb4',
-  versionLong: '0.0.2-512ecb4',
+    version: '0.0.3',
+    name: 'ionic-react-kit',
+    versionDate: '2025-06-10T11:09:38.662Z',
+    description: 'An Ionic React Starter Kit bundled with care.',
+    gitCommitHash: 'c173896',
+    versionLong: '0.0.3-c173896',
 };
 export default versions;
